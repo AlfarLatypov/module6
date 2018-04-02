7c8aadbc15d9ec639588107a1b37ba6cfce9a61c
ЛатыповА <@223_9>
2018-04-02 22:06:19 +0600

    Add project files.

---
+++ Bankomat/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KKB.Bank.Module;
+using GeneratorName;
+
+namespace Bankomat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Client> ListClient = new List<Client>();
+            GeneratorName.Generator g = new Generator();
+
+            Client c1 = new Client();
+            c1.DoB = DateTime.Now.AddYears(-60);
+            c1.fullName = g.GenerateDefault(Gender.man);
+            c1.IIN = "123456789123";
+            c1.Login = "Qwerty";
+            c1.Password = "123";
+            c1.PhoneNumber = "87777730111";
+
+            ListClient.Add(c1);
+
+        }
+    }
+}
---
+++ KKB.Bank/Module/Client.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KKB.Bank.Module
+{
+    public class Client
+
+    {
+
+        public Client()
+        {
+            ListAccount = new  List<Account>();
+    }
+
+        private string fullName_;
+        public string fullName {
+
+            get { return fullName_; }
+            
+            set { fullName_ = value.Replace("", "") }
+
+
+        }
+
+        private string IIN_;
+        public string IIN { get { return IIN_; }
+            set {
+                if (value.Length == 12) IIN = value;
+                else throw new Exception("\nError! Некорректно введен ИИН!");
+            }
+        }
+
+        public DateTime DoB { get; set; }
+
+        public string Login { get; set; }
+        public string Password { private get; set; }
+        public string PhoneNumber { get; set; }
+
+        List<Account> ListAccount;
+
+
+        public void ClientInfoPrint()
+        {
+            Console.WriteLine();
+        }
+
+    }
+}
